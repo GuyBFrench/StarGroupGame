b74529b393db20fedf395191f5c8e5a729e013bc
Guy French <guybfrench@gmail.com>
2024-04-09 22:14:19 -0600

    testing and organization

---
+++ DungeonCrawler/Assets/Scripts/Character/TriggerEventsBehaviour.cs
@@ -0,0 +1,37 @@
+
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Brady
+{
+    public class TriggerEventsBehaviour : MonoBehaviour
+    {
+
+        public UnityEvent onEnemyDamage, onPlayerDamage;
+
+        private Rigidbody rigidEnemy;
+
+        private void Awake()
+        {
+            rigidEnemy = GetComponent<Rigidbody>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Debug.Log("contact");
+            if (other.gameObject.CompareTag("Player"))
+            {
+               onPlayerDamage.Invoke(); 
+            }
+
+            if (other.gameObject.CompareTag("Sword"))
+            {
+             onEnemyDamage.Invoke();
+            }
+            
+            
+        }
+        
+    }    
+}
+
