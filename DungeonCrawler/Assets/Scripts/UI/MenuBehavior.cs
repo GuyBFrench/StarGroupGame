e6deae8cb2d3aa6c491eae8ace9fcc68817a6f30
Amanda <10932525@uvu.edu>
2024-04-15 11:32:56 -0600

    Added a collider to the cellar. And edited the menu

--- DungeonCrawler/Assets/Scripts/UI/MenuBehavior.cs
+++ DungeonCrawler/Assets/Scripts/UI/MenuBehavior.cs
@@ -3,15 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class MenuBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        void Play()
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
-    }
-
     public void Quit()
     {
         Debug.Log("Quit!");
