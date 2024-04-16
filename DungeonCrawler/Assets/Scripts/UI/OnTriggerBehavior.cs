55acec6ad4b0349724f4c847f6f10dac6906c0a2
Amanda <10932525@uvu.edu>
2024-04-16 04:29:01 -0600

    Added Open Button. Started OnTriggerStay

---
+++ DungeonCrawler/Assets/Scripts/UI/OnTriggerBehavior.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerBehavior : MonoBehaviour
+{
+    public void OnTriggerStay(Collider other)
+    {
+        
+    }
+
+}
