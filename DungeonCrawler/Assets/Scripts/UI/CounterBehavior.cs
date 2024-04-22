5912a37899a18188996dbd5c083c786c86ccd63a
Amanda <10932525@uvu.edu>
2024-04-22 02:35:08 -0600

    Worked on getting max value to work for levels

--- DungeonCrawler/Assets/Scripts/UI/CounterBehavior.cs
+++ DungeonCrawler/Assets/Scripts/UI/CounterBehavior.cs
@@ -3,16 +3,21 @@ using UnityEngine.UI;
 
 public class CounterBehavior : MonoBehaviour
 {
-    public IntData intData; // Assign your IntData asset to this field in the Inspector
+    public IntData intData; // Assign the IntData asset in the Inspector
+    public int maxValue = 5; // Set the maximum value
 
     private void Start()
     {
-        GetComponent<Button>().onClick.AddListener(IncrementValue);
+        Button button = GetComponent<Button>();
+        if (button != null)
+        {
+            button.onClick.AddListener(IncrementValue);
+        }
     }
 
     private void IncrementValue()
     {
-        intData.UpdateValue(1); // Increment the value by 1 on each button click
-        intData.CheckMaxValue(4); // Check for the maximum value of 4
+        intData.UpdateValue(1); // Increment the value by 1
+        intData.CheckMaxValue(maxValue); // Check for the maximum value
     }
 }
